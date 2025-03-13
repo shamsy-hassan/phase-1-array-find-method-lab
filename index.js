37c4e08cc608cb4bc0a76960f95a500dfad28a98
shamsy-hassan <shamsyhassan254@gmail.com>
2025-03-13 23:46:39 +0300

    asignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// code your solution here
+// code solution
+function superbowlWin(record) {
+    const win = record.find((game) => game.result === "W");
+    return win ? win.year : undefined;
+  }
+  superbowlWin(record);
